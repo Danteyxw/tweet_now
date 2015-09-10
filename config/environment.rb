d192368c81bfabc24e1f01675bfb243c7ba7e88c
Dante Wan <danteyxw@outlook.com>
2015-09-10 17:43:15 +0800

    uncomment APP_KEY

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@
 #      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec)
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Require gems we care about
@@ -31,7 +34,7 @@ require 'omniauth-twitter'
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
-# APP_KEY = YAML.load_file('config/keys.yml')
+APP_KEY = YAML.load_file('config/keys.yml')
 
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
